e161f22a13746ef84b9d7bdb4f2ff2db22e790f4
Maayan Heller <maayan.heller@gmail.com>
2023-04-09 02:11:29 +0300

    Added navigation, and gave app option to ask forcamera premmisions

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { StyleSheet, View } from 'react-native';
+
+import Home from './Components/Home';
+import Camera from './Components/CameraPage';
+import PhotoPreview from './Components/PhotoPreview';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={Home}
+          />
+
+          <Stack.Screen
+            name="Camera"
+            component={Camera}
+          />
+        </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
